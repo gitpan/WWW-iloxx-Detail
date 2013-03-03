54463a2ade753e7c6d36b7261b2cbbdd3e562f00
- <stefanos@cpan.org>
2013-03-03 01:31:33 +0000

    Import of STEFANOS/WWW-iloxx-Detail-0.2 from CPAN.
    gitpan-cpan-distribution: WWW-iloxx-Detail
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         STEFANOS/WWW-iloxx-Detail-0.2.tar.gz
    gitpan-cpan-author:       STEFANOS
    gitpan-cpan-maturity:     released

--- Detail.pm
+++ Detail.pm
@@ -4,7 +4,7 @@ use strict;
 use Exporter;
 our @ISA = qw/Exporter/;
 our @EXPORT = qw/iloxxcheck/;
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 use LWP::Simple;
 use LWP::UserAgent;
 
@@ -92,12 +92,9 @@ WWW::iloxx::Detail - Perl module for the iloxx online tracking service with deta
 
 =head1 AUTHOR
 
-    Stefan Gipper <stefanos@cpan.org>, http://www.coder-world.de/
+    -
 
 =head1 COPYRIGHT
-
-	WWW::iloxx::Detail is Copyright (c) 2010 Stefan Gipper
-	All rights reserved.
 
 	This program is free software; you can redistribute
 	it and/or modify it under the same terms as Perl itself.
